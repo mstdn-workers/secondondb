60c9cbc5a0c113955703115fcfbf902dbbb39f36
rotten-hide <hidetake6991@gmail.com>
2018-01-29 02:16:28 +0900

    カッコつかないバグfix

--- get_stream/app/main.py
+++ get_stream/app/main.py
@@ -5,5 +5,4 @@ if __name__ == "__main__":
     mstdn = h1don.h1don()
     executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
     executor.submit(mstdn.get_LTL_stream())
-    executor.submit(mstdn.heartbeat_check()
-    
+    executor.submit(mstdn.heartbeat_check())
