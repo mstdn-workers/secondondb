e8538dc4ae492edb637117f3a43a1805ec94dd93
rotten-hide <hidetake6991@gmail.com>
2018-01-30 22:25:39 +0900

    mastodon.pyでリファクタ

--- get_stream/app/main.py
+++ get_stream/app/main.py
@@ -1,25 +1,24 @@
-import h1don
 import os
 from mastodon import Mastodon, StreamListener
 
-
 client_name = os.getenv("CLIENT_NAME", "CLIENT")
-access_token = os.getenv("ACCESS_TOKEN", "")
-api_base_url = os.getenv("API_BASE_URL", "")
-mastodon_user = os.getenv("MASTODON_USER", "")
-mastodon_pass = os.getenv("MASTODON_PASS", "")
-client_credfile_name = "clientcred.txt"
-user_credfile_name = "usercred.txt"
+api_base_url = os.getenv("API_BASE_URL", "---")
+mastodon_user = os.getenv("MASTODON_USER", "---")
+mastodon_pass = os.getenv("MASTODON_PASS", "---")
+client_credfile_name = "client.mstdncred"
+user_credfile_name = "user.mstdncred"
 
 Mastodon.create_app(client_name, api_base_url = api_base_url, to_file = client_credfile_name)
 mastodon = Mastodon(client_id=client_credfile_name, api_base_url = api_base_url)
 mastodon.log_in(mastodon_user, mastodon_pass,to_file = user_credfile_name)
 
+
+# dict_keys(['id', 'created_at', 'in_reply_to_id', 'in_reply_to_account_id', 'sensitive', 'spoiler_text', 'visibility', 'language', 'uri', 'content', 'url', 'reblogs_count', 'favourites_count', 'reblog', 'application', 'account', 'media_attachments', 'mentions', 'tags', 'emojis'])
+
 class MyStreamListener(StreamListener):
     def __init__(self):
         super(MyStreamListener, self).__init__()
-        # self.logger = logging.getLogger(self.__class__.__name__)
-    
+
     def handle_stream(self, response):
         try:
             super().handle_stream(response)
@@ -28,15 +27,13 @@ class MyStreamListener(StreamListener):
             raise
     
     def on_update(self, status):
-        # self.logger.info(status_info_string(status))
-        print("update: "+status)
+        print("update: "+str(status['id']))
         pass
 
     def on_delete(self, status_id):
-        # self.logger.info(f"status delete_event: {status_id}")
-        print("delete: "+status_id)
+        print("delete: "+str(status_id))
         pass
 
 if __name__ == "__main__":
-    listener = QueueStreamListener(status_queue)
+    listener = MyStreamListener()
     mastodon.stream_local(listener)
