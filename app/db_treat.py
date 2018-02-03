2bd9199a42f5cdd364e11f3d7e981e7adc05c5e3
rotten-hide <hidetake6991@gmail.com>
2018-02-03 17:49:57 +0900

    セミコロン入れ忘れ修正

--- app/db_treat.py
+++ app/db_treat.py
@@ -2,45 +2,51 @@ import psycopg2
 
 class db_treat:
     def __init__(self, dsn_dict):
-        self.connection = psycopg2.connect(
-            dbname=dsn_dict['dbname'], 
-            user=dsn_dict['user'], 
-            password=dsn_dict['password'], 
-            host=dsn_dict['host'], 
-            port=dsn_dict['port'])
-        self.cursor = self.connection.cursor()
-    
-    def close(self):
-        self.cursor.close()
-        self.connection.close()
+        self.dsn_dict = dsn_dict
 
     def insert(self, id, json_str):
-        self.cursor.execute("INSERT INTO localtimeline VALUES (%s, %s)", [id, json_str])
-        self.connection.commit() 
+        with psycopg2.connect(
+            dbname=self.dsn_dict['dbname'], 
+            user=self.dsn_dict['user'], 
+            password=self.dsn_dict['password'], 
+            host=self.dsn_dict['host'], 
+            port=self.dsn_dict['port']) as conn:
+            cursor = conn.cursor()
+            sql = "INSERT INTO localtimeline VALUES (" + id + ", '" + json_str +"');"
+            print(sql)
+            cursor.execute(sql)
+            conn.commit()
 
     def insert_all(self, status):
-        # self.cursor.execute("INSERT INTO localtimeline VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", [
-        print( status.keys() )
-        keys = ""
-        values = ""
-        kc = 0
-        vc = 0
-        for k in status.keys():
-            if keys == "":
-                keys = k
-                values = str(status[k])
-                kc = kc + 1
-                vc = vc + 1
-            else:
-                keys = keys + ", " + k
-                values = values + ", '" + str(status[k]) + "'" 
-                kc = kc + 1
-                vc = vc + 1
-        print(str(kc)+"keys: "+keys)
-        print(str(vc)+"values: "+values)
-        sql = "INSERT INTO localtimeline (" + keys + ") VALUES (" + values + ");"
-        self.cursor.execute(sql)
-        self.connection.commit()
+        with psycopg2.connect(
+            dbname=self.dsn_dict['dbname'], 
+            user=self.dsn_dict['user'], 
+            password=self.dsn_dict['password'], 
+            host=self.dsn_dict['host'], 
+            port=self.dsn_dict['port']) as conn:
+            cursor = conn.cursor()
+            # self.cursor.execute("INSERT INTO localtimeline VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", [
+            print( status.keys() )
+            keys = ""
+            values = ""
+            kc = 0
+            vc = 0
+            for k in status.keys():
+                if keys == "":
+                    keys = k
+                    values = str(status[k])
+                    kc = kc + 1
+                    vc = vc + 1
+                else:
+                    keys = keys + ", " + k
+                    values = values + ", '" + str(status[k]) + "'" 
+                    kc = kc + 1
+                    vc = vc + 1
+            print(str(kc)+"keys: "+keys)
+            print(str(vc)+"values: "+values)
+            sql = "INSERT INTO localtimeline (" + keys + ") VALUES (" + values + ");"
+            cursor.execute(sql)
+            conn.commit()
 
 
         
