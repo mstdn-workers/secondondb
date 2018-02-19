67439c5b624f1d3b6fe27b86ca5204372b50ad0c
rotten-hide <hidetake6991@gmail.com>
2018-02-19 22:23:32 +0900

    fix

--- app/BULB/BULB/settings.py
+++ app/BULB/BULB/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'BULB.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': 'TANK',
+        'PORT': '5432',
     }
 }
 
