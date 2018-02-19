e576c04748538fe19101779cf9d4456167d44986
rotten-hide <hidetake6991@gmail.com>
2018-02-19 21:11:31 +0900

    bulb gunicorn setup

--- app/BULB/BULB/settings.py
+++ app/BULB/BULB/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4*3*emm2goei^_7__#t5ao@%^0f_lk$==voho5w#&g9&+4!k8h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bulb_api',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja-JP'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
