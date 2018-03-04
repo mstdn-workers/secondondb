110db919f7da061eef2adc79d27d168e2a37303e
natsukirigakure <natsuki@kirigakure.net>
2018-03-04 14:52:22 +0900

    mod: BUCKETカラム追加

--- BUCKET/init/01_createdb.sql
+++ BUCKET/init/01_createdb.sql
@@ -1,5 +1,17 @@
 CREATE TABLE localtimeline (
   id TEXT PRIMARY KEY,
-  json_str TEXT
+  json_str TEXT NOT NULL,
+  account_id TEXT,
+  in_reply_to_id TEXT,
+  in_reply_to_account_id TEXT,
+  reblog TEXT,
+  content TEXT,
+  created_at TIMESTAMP [3],
+  sensitive TEXT,
+  spoiler_text TEXT,
+  visibility TEXT,
+  media_attachments_id TEXT[],
+  mentions_id TEXT[],
+  tags_name TEXT[]
 );
 
