609de2f9d6b0b92f218235968ca315d238b39678
natsukirigakure <natsuki@kirigakure.net>
2018-03-22 21:10:23 +0900

    破壊的変更

--- TANK/init/01_createdb.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE sprinkler_users (
-  id TEXT PRIMARY KEY,
-  user_id TEXT,
-  hook_url TEXT,
-  hook_secret TEXT
-);
--- app/BULB/bulb_api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-app_name = 'bulb_api'
-urlpatterns = [
-    url('', views.index, name='index'),
-]
